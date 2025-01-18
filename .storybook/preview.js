c153371f35493876e291d91b8dedfbc128f3ba06
Victoria240 <taiwovictoria024@gmail.com>
2025-01-18 19:47:05 +0000

    organised my files and moved global styles to global.css. Also created a folder for buttons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,4 @@
 /** @type { import('@storybook/react').Preview } */
-
-// Import Font Awesome and add icons to the library
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-
-// Add Font Awesome icons to the library
-library.add(fas, far);
-
-// Define Storybook preview configuration
 const preview = {
   parameters: {
     controls: {
@@ -19,5 +9,16 @@ const preview = {
     },
   },
 };
+
+// Import global styles
+import '../src/styles/global.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+// Add icons to the library
+library.add(fas, far);
+
 
 export default preview;
